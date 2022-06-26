f972ecf27a052a4c5d6b6e51ed603cd8ef2b2c94
pradeep kumar swain <54628915+pradeep2165@users.noreply.github.com>
2022-06-26 10:17:41 +0530

    validation added

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,10 +1,21 @@
 const express = require("express");
 const router = express.Router();
 const User = require("../models/Users");
-router.get("/", (req, res) => {
-  console.log(req.body);
-  const user = User(req.body);
-  user.save();
-  res.send(req.body);
+const { body, validationResult } = require("express-validator");
+router.post("/signup", [body("name", "Name should be min 3 chars long").isLength({ min: 3 }), body("email", "Enter a valid email").isEmail(), body("password", "Password should be min 5 chars long").isLength({ min: 5 })], (req, res) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
+  }
+  User.create({
+    name: req.body.name,
+    email: req.body.email,
+    password: req.body.password,
+  })
+    .then((user) => res.json(user))
+    .catch((err) => {
+      console.log(err);
+      res.json({ error: "Please enter a unique value for email", message: err.message });
+    });
 });
 module.exports = router;
