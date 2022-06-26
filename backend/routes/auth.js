cda0fc3fccb5fbfa6b79f344c7c709aa385dc27b
pradeep kumar swain <54628915+pradeep2165@users.noreply.github.com>
2022-06-26 10:36:20 +0530

    created signup endpoint

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -2,20 +2,25 @@ const express = require("express");
 const router = express.Router();
 const User = require("../models/Users");
 const { body, validationResult } = require("express-validator");
-router.post("/signup", [body("name", "Name should be min 3 chars long").isLength({ min: 3 }), body("email", "Enter a valid email").isEmail(), body("password", "Password should be min 5 chars long").isLength({ min: 5 })], (req, res) => {
+router.post("/signup", [body("name", "Name should be min 3 chars long").isLength({ min: 3 }), body("email", "Enter a valid email").isEmail(), body("password", "Password should be min 5 chars long").isLength({ min: 5 })], async (req, res) => {
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
     return res.status(400).json({ errors: errors.array() });
   }
-  User.create({
-    name: req.body.name,
-    email: req.body.email,
-    password: req.body.password,
-  })
-    .then((user) => res.json(user))
-    .catch((err) => {
-      console.log(err);
-      res.json({ error: "Please enter a unique value for email", message: err.message });
+  try {
+    let user = await User.findOne({ email: req.body.email });
+    if (user) {
+      return res.status(400).json({ error: "Sorry a user with this email already exits" });
+    }
+    user = await User.create({
+      name: req.body.name,
+      email: req.body.email,
+      password: req.body.password,
     });
+    res.json(user);
+  } catch (err) {
+    console.error(err.message);
+    res.json({ error: "Please enter a unique value for email", message: err.message });
+  }
 });
 module.exports = router;
