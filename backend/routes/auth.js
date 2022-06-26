ba6c9b065e93d8d84e7f44cc70fcc0b19729d676
pradeep kumar swain <54628915+pradeep2165@users.noreply.github.com>
2022-06-26 10:44:32 +0530

    implemented secure password

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const router = express.Router();
 const User = require("../models/Users");
+const bcrypt = require("bcryptjs");
+
 const { body, validationResult } = require("express-validator");
 router.post("/signup", [body("name", "Name should be min 3 chars long").isLength({ min: 3 }), body("email", "Enter a valid email").isEmail(), body("password", "Password should be min 5 chars long").isLength({ min: 5 })], async (req, res) => {
   const errors = validationResult(req);
@@ -12,10 +14,14 @@ router.post("/signup", [body("name", "Name should be min 3 chars long").isLength
     if (user) {
       return res.status(400).json({ error: "Sorry a user with this email already exits" });
     }
+    //secure password by bcrypt
+    const salt = await bcrypt.genSalt(10);
+    secPass = await bcrypt.hash(req.body.password, salt);
+
     user = await User.create({
       name: req.body.name,
       email: req.body.email,
-      password: req.body.password,
+      password: secPass,
     });
     res.json(user);
   } catch (err) {
